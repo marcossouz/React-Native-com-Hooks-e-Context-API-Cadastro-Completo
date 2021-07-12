3db602daaf77a343f575f296ce5e8a88caaaf495
Marcos Souza <marcossouza@Marcoss-MacBook-Air.local>
2021-07-12 07:52:53 -0300

    Lista de usuários #01
    Signed-off-by: Marcos Souza <marcossouza@Marcoss-MacBook-Air.local>

---
+++ rncrud/src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    name: "João Silva",
+    email: "josil@empmail.com",
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png'
+  },
+  {
+    id: 2,
+    name: "Mariana Aguiar",
+    email: "riamasil@empmail.com",
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635456_960_720.png'
+  },
+  {
+    id: 3,
+    name: "Julia Fragoso",
+    email: "julfra@empmail.com",
+    avatarUrl: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'
+  },
+  {
+    id: 4,
+    name: "Rafael Monteiro",
+    email: "rafmont@empmail.com",
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png'
+  },
+]
--- rncrud/src/views/UserList.js
+++ rncrud/src/views/UserList.js
@@ -1,8 +1,37 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
+import { ListItem, Avatar } from 'react-native-elements'
+import users from '../data/users'
 
 export default props => {
+  function getUserItem({ item: user }) {
+    return (
+      <>
+        <ListItem
+          key={user.id}
+          onPress={() => props.navigation.navigate('UserForm')}
+          bottomDivider>
+          <Avatar
+            size={64}
+            rounded
+            title={user.name[0]}
+            source={ user.avatarUrl ? {uri: user.avatarUrl} : null } />
+          <ListItem.Content>
+            <ListItem.Title>{user.name}</ListItem.Title>
+            <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+          </ListItem.Content>
+        </ListItem>
+      </>
+    )
+  }
+
   return (
-    <Text>UserList</Text>
+    <View>
+      <FlatList
+        keyExtractor={user => user.id.toString()}
+        data={users}
+        renderItem={getUserItem}
+      />
+    </View>
   )
 }
